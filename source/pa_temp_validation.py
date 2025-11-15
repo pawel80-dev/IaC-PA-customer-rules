2e017db1afdf48805080f6db74878f92140cd615
Pawel <pawel80@gmail.com>
2025-11-15 17:43:30 +0100

    pa_rules_v10

--- source/pa_temp_validation.py
+++ source/pa_temp_validation.py
@@ -1,8 +1,14 @@
 from excel_api import validate_template
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("ex_file_path", type=str, help="Excel file path")
+parser.add_argument("ex_sheet", type=str, help="Excel sheet name")
+args = parser.parse_args()
 
 
 def main() -> None:
-    validate_template("customer_rules/customer_A.xlsx", sheet_name="Rules")
+    validate_template(args.ex_file_path, args.ex_sheet)
 
 
 if __name__ == "__main__":
