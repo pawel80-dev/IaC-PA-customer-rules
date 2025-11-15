4f2764e5660988d7ade7167910136b09c77fc6f1
Pawel <pawel80@gmail.com>
2025-11-15 16:33:40 +0100

    pa_rules_v04

--- source/pa_current_config.py
+++ source/pa_current_config.py
@@ -1,9 +1,14 @@
 from pa_api import display_sec_policies
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("pa_api_url", type=str, help="PA API URL")
+parser.add_argument("pa_api_key", type=str, help="PA API key")
+args = parser.parse_args()
+
 
 def main() -> None:
-    policies = display_sec_policies(api_url, pa_api_key)
-    # print(policies["result"]["entry"])
-    # print("CURRENT POLICIES:")
+    policies = display_sec_policies(args.pa_api_url, args.pa_api_key)
     for policy in policies["result"]["entry"]:
         print(policy["@name"], policy["from"]["member"], policy["source"]["member"], 
               policy["to"]["member"], policy["destination"]["member"], 
