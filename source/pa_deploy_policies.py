c80779671d66428dfc5c0c3126fd5c0c5cb1d84c
Pawel <pawel80@gmail.com>
2025-11-19 19:51:09 +0100

    pa_rules_v17

--- source/pa_deploy_policies.py
+++ source/pa_deploy_policies.py
@@ -1,4 +1,4 @@
-from pa_api import create_obj_services
+from pa_api import create_obj_services, create_obj_addresses
 from excel_api import create_config
 import argparse
 
@@ -14,6 +14,12 @@ def main() -> None:
     conf = create_config(args.ex_file_path, sheet_name=args.ex_sheet)
     for service in conf["services"]:
         create_obj_services(args.pa_api_url, args.pa_api_key, service["name"], service["protocol"], service["port"], "")
+
+    for addr_group in conf["addr_groups"]:
+        for addr_object in addr_group["objects"]:
+            for obj_name, obj_value in addr_object.items():
+                create_obj_addresses(args.pa_api_url, args.pa_api_key, obj_name, obj_value, "")
+                # create_obj_addresses(args.pa_api_url, args.pa_api_key, "server2", "10.0.0.2/32", "Server 2")
 
     # services = display_obj_services(api_url, pa_api_key)
     # for service in services["result"]["entry"]:
